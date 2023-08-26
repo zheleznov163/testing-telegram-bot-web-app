d3a5596f958d08c97c8e8f9cedb73e7ab0686f1d
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2023-08-26 20:31:00 +0300

    build: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+    useEffect(() => {
+        Telegram.WebApp.ready();
+    }, []);
+
+    const close = useCallback(() => {
+        Telegram.WebApp.close();
+    }, []);
+
+    return (
+        <div className="App">
+            work
+            <button onClick={close}>Закрыть</button>
+        </div>
+    );
 }
-
-export default App;
