fd62511b87a064fc75952fc38592f29c26aa771a
Nikita Zheleznov <zheleznov.frontend@gmail.com>
2023-08-27 15:27:10 +0300

    feat(moleculs): + Header

---
+++ src/components/moleculs/Header/Header.tsx
@@ -0,0 +1,21 @@
+// import from 'react'
+
+import { DetailedHTMLProps, HTMLAttributes } from "react";
+import { Button } from "../../atoms";
+
+type Props = DetailedHTMLProps<
+    HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+> & {
+    onClickButton(): void;
+    username: string;
+};
+
+export default function Header({ onClickButton, username, ...props }: Props) {
+    return (
+        <div {...props} className={"header " + props.className}>
+            <Button onClick={onClickButton} />
+            <span className="username">{username}</span>
+        </div>
+    );
+}
